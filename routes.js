391ec0d2d820f6d0b3a2defc4de8b847c9e704d1
Soham Marik <soham.m15@iiits.in>
2018-07-13 19:44:38 +0530

    Add link to blog template

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 exports.setup = function (app) {
     var home = require('./Controller/home');
+    var blog = require('./Controller/blog');
     app.get('/', home.index);
+    app.get('/blog',blog.index);
     app.post('/send-email', home.sendMail);
 }
